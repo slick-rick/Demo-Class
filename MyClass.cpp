f89151f5a8e3d57c3e8d9ca3c91232081d775a43
slick-rick <slick-rick@users.noreply.github.com>
2016-09-22 14:28:21 -0400

    Filled in missing parts of main and Implementation file

--- MyClass.cpp
+++ MyClass.cpp
@@ -1,1 +1,31 @@
+//Implementation File
 
+void MyClass::setNum (int v)
+{
+    num = v;
+}
+
+int MyClass::getNum ()
+{
+  return num;
+}
+
+void MyClass::setRoom (string x)
+{
+    room = x;
+}
+
+string MyClass::getRoom ()
+{
+  return room;
+}
+
+void MyClass::setfun (bool y)
+{
+     fun = y;
+}
+
+bool MyClass::getfun ()
+{
+  return fun;
+}
--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 //complete me
 
+#include "MyClass.h"
+#include <string>
 #include <iostream>
 using namespace std;
 
 
 int main()
 {
+ MyClass myvar;
+ Myclass myvar2;
+ int var = 10;
+ string str;
+
+ cin>>var;
+ cout<<"var = "<< var <<endl;
+
+ cin>>str;
+ cout<<"str = "<< str;
+ myvar = myvar2;
+
+ myvar.setNum(32);
+ myvar.getNum(); 
 
 }
